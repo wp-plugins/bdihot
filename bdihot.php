8c0188e776e386bfe7030bfc68e5be616811168b
ramiy <ramiy@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-05-19 05:54:11 +0000

    0.2
    git-svn-id: https://plugins.svn.wordpress.org/bdihot/tags/0.2@546125 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ bdihot.php
@@ -0,0 +1,158 @@
+<?php
+/**
+ * Plugin Name: Random Joke (Bdihot.co.il)
+ * Plugin URI: http://www.bdihot.co.il/wordpress_plugin/
+ * Description: This plugin allows you to add a random joke (from bdihot.co.il) using wordpress Sidebar-Widget and Dashboard-Widget systems.
+ * Version: 0.2
+ * Author: Rami Y
+ * Author URI: http://www.bdihot.co.il/
+ * License: GPL2
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+**/
+
+
+// Uninstall hook
+register_uninstall_hook( __FILE__ , 'bdihot_uninstall' );
+
+// Load translations
+add_action( 'init', 'bdihot_translations' );
+
+// Register dashboard widget
+add_action( 'wp_dashboard_setup', 'bdihot_add_dashboard_widget' );
+
+// Register sidebar widget
+add_action( 'widgets_init', 'bdihot_add_sidebar_widget' );
+
+
+
+/*
+ * General
+ */
+
+
+ 
+// Uninstall bdihot
+function bdihot_uninstall() {
+	delete_option('bdihot_random_joke');
+}
+
+// Load translation POT files
+function bdihot_translations() {
+	load_plugin_textdomain( 'bdihot', null, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+}
+
+// Set/Get plugin options
+function bdihot_options() {
+	$defaults = array(
+		'title' => __( 'Random Joke', 'bdihot' ),
+		'jokes_number' => 1,
+	);
+	if ( ( !$options = get_option( 'bdihot_random_joke' ) ) || !is_array( $options ) )
+		$options = array();
+	return array_merge( $defaults, $options );
+}
+
+// Cache feed for 2 seconds
+function bdihot_feed_cache_lifetime() {
+	return 2;
+}
+
+
+
+/*
+ * Dashboard Widget
+ */
+
+
+
+// Register dashboard widget
+function bdihot_add_dashboard_widget() {
+	wp_add_dashboard_widget( 'bdihot_dashboard_random_joke', __( 'Random Joke', 'bdihot' ), 'bdihot_widget_output' );
+}
+
+
+// The content of the widget (random joke)
+function bdihot_widget_output() {
+	include_once( ABSPATH . WPINC . '/feed.php' );
+
+	$widget_options = bdihot_options();
+	$url = 'http://www.bdihot.co.il/webmasters-rss/';
+
+	add_filter( 'wp_feed_cache_transient_lifetime' , 'bdihot_feed_cache_lifetime', $url );
+	$rss = fetch_feed( $url );
+	remove_filter( 'wp_feed_cache_transient_lifetime' , 'bdihot_feed_cache_lifetime', $url );
+
+	if ( ! is_wp_error( $rss ) ) :
+		$max_items = $rss->get_item_quantity( $widget_options['jokes_number'] );	// Total items, default one.
+		$rss_items = $rss->get_items( 0, $max_items );								// Build an array, starting with 0.
+	endif;
+
+	if ( $max_items == 0 ) {
+		echo __('No Jokes found.', 'bdihot');
+    } else {
+		foreach ( $rss_items as $item ) : 
+			echo '<p><a href="' . esc_url( $item->get_permalink() ) . '" title="' . $item->get_date( 'j F Y | g:i a' ) .'" target="_blank">' . esc_html( $item->get_title() ) . '</a></p>';
+			echo wpautop( $item->get_description() );
+		endforeach;
+	}
+}
+
+
+
+/*
+ * Sidebar Widget
+ */
+
+
+
+// Register sidebar widget
+function bdihot_add_sidebar_widget() {
+	register_widget( 'Bdihot_Widget' );
+}
+
+class Bdihot_Widget extends WP_Widget {
+
+	// Register widget
+	public function __construct() {
+		parent::__construct(
+	 		'bdihot_widget',
+			__( 'Random Joke', 'bdihot' ),
+			array( 'description' => __( 'Displays Random Joke from Bdihot.co.il', 'bdihot' ), )
+		);
+	}
+
+	// Widget front-end display
+	public function widget( $args, $instance ) {
+		extract( $args );
+		$title = apply_filters( 'widget_title', $instance['title'] );
+		echo $before_widget;
+		if ( ! empty( $title ) ) echo $before_title . $title . $after_title;
+		bdihot_widget_output();
+		echo $after_widget;
+	}
+
+	// Sanitize widget form values as they are saved
+	public function update( $new_instance, $old_instance ) {
+		$instance = array();
+		$instance['title'] = strip_tags( $new_instance['title'] );
+		return $instance;
+	}
+
+	// Widget back-end form
+	public function form( $instance ) {
+		if ( isset( $instance[ 'title' ] ) ) {
+			$title = $instance[ 'title' ];
+		}
+		else {
+			$title = __( 'Random Joke', 'bdihot' );
+		}
+		?>
+		<p>
+		<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:', 'bdihot' ); ?></label>
+		<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+		</p>
+		<?php
+	}
+}
+
+?>
---
+++ readme.txt
@@ -0,0 +1,35 @@
+=== Random Joke (Bdihot.co.il) ===
+Contributors: Rami Y
+Donate link: http://bdihot.co.il/
+Tags: bdihot, humor, jokes, random, joke
+Requires at least: 3.0
+Tested up to: 3.3
+Stable tag: trunk
+
+This plugin allows you to add a random joke (from bdihot.co.il) using wordpress Sidebar-Widget and Dashboard-Widget systems.
+
+== Description ==
+
+This plugin allows you to add a random joke (from bdihot.co.il) using wordpress Sidebar-Widget and Dashboard-Widget systems.
+
+== Installation ==
+
+It's a pretty straightforward little plugin.
+
+1. Upload plugin folder to the `/wp-content/plugins/` directory.
+2. Activate the plugin through the 'Plugins' menu in WordPress.
+3. Visit your Dashboard to configure and enjoy the new widget.
+
+== Screenshots ==
+
+1. Random Joke on WordPress Dashboard.
+2. Sidebar widget sittings.
+3. Random Joke on TwentyTen sidebar.
+
+== Changelog ==
+
+= 0.2 (07.04.2012) =
+* Added Dashboard-Widget.
+
+= 0.1 (01.01.2012) =
+* Initial release.
